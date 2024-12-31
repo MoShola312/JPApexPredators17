ce148e947694f47fd784500fb93ef074c2155cee
Mohammed Sholaja <mohammed.sholaja@gmail.com>
2024-12-31 10:29:52 -0600

    Initial Commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by mohammed sholaja on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JPApexPredators17/Predators.swift
@@ -0,0 +1,63 @@
+//
+//  Predators.swift
+//  JPApexPredators17
+//
+//  Created by mohammed sholaja on 10/31/24.
+//
+
+import Foundation
+
+class Predators {
+    var allApexPredators: [ApexPredator] = []
+    var apexPredators: [ApexPredator] = []
+    
+    init() {
+        decodeApexPredatorData()
+    }
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                allApexPredators = try decoder.decode([ApexPredator].self, from: data)
+                apexPredators = allApexPredators
+            } catch {
+                print("Error decoding JSON data: \(error)")
+            }
+        }
+    }
+    
+    func search(for searchTerm: String) -> [ApexPredator] {
+        if searchTerm.isEmpty {
+            //if the search field is empty return the whole list
+            return apexPredators
+        } else {
+            return apexPredators.filter{
+                //ignores type case the user types
+                predator in predator.name.localizedCaseInsensitiveContains(searchTerm)
+            }
+        }
+    }
+    
+    func sort(by alphabetical: Bool) {
+        apexPredators.sort { predator1, predator2 in
+            if alphabetical {
+                predator1.name < predator2.name
+            } else {
+                predator1.id < predator2.id
+            }
+        }
+    }
+        
+    func filter(by type: PredatorType) {
+        if type == .all {
+            apexPredators = allApexPredators
+        } else {
+            apexPredators = allApexPredators.filter { predator in predator.type == type
+            }
+        }
+    }
+}
+
